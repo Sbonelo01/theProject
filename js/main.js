64a6d358e891342cea5dbb5eef9407d745f032c0
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-11-19 20:42:22 +0200

    styled

--- js/main.js
+++ js/main.js
@@ -1,36 +1,32 @@
-'use strict';
+"use strict"
 
-// the corousel code
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// the controls for next and previous
-let plusSlides = (n) => {
-	showSlides(slideIndex += n)
-} 
-
-// the controls for the thumbnails
-let currentSlide = (n) => {
-	showSlides(slideIndex = n)
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-const showSlides = (n) => {
-	var i;
-	var slides = document.getElementsByClassName('slides')
-	var dots = document.getElementsByClassName('demo')
-	if(n > slides.length){
-		slideIndex = 1
-	}
-	if(n < 1){
-		slideIndex = slides.length
-	}
-	for(i = 0; i < slides.length; i++){
-		slides[i].style.display = 'none';
-	}
-	for(i = 0; i < dots.length; i++){
-		dots[i].className = dots[i].className.replace(' active', '')
-	}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-	slides[slideIndex-1].style.display = 'block';
-	dots[slideIndex-1].className += ' active';
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  // captionText.innerHTML = dots[slideIndex-1].alt;
 }
